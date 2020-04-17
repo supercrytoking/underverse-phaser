ffc41de581329c3bf5fc72a5747677c6f75de141
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2020-04-17 16:54:00 -0400

    200 mobs!

--- src/Scenes/GameScene.ts
+++ src/Scenes/GameScene.ts
@@ -82,7 +82,7 @@ export class GameScene extends Phaser.Scene {
         // });
 
         this.mobs = this.add.group();
-        for (var i = 0; i < 100; i++) {
+        for (var i = 0; i < 200; i++) {
             var mob = new Mob(this, this.player.x + R.int(-1000, 1000), this.player.y + R.int(-1000, 1000), 'MOB_DUNNOT', 100, 100, 100)
             this.mobs.add(mob);
         }
