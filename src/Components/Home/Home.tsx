f00af12c755daf8eac1eb9e3151cb3cf477ce19c
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2020-04-20 06:26:50 -0400

    Added a homepage

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Home.css'
+
+type HomeProps = {}
+type HomeState = {}
+export default class Home extends React.Component<HomeProps, HomeState> {
+    constructor(props: HomeProps) {
+        super(props)
+
+        this.state = {}
+    }
+
+    render() {
+        return(
+            <div className="home">
+                <Link to={'/game'}>Game</Link><br/>
+                <Link to={'/login'}>Login</Link><br/>
+                <Link to={'/register'}>Register</Link><br/>
+                <Link to={'/login/forgot'}>Forgot Password?</Link><br/>
+                <Link to={'/logout'}>Logout</Link>
+            </div>
+        )
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'reset-css'
 import './index.css'
 
+import Home from './Components/Home/Home'
 import Game from './Components/Game/Game'
 import AuthenticationModal from './Components/AuthenticationModal/AuthenticationModal'
 import Logout from './Components/Logout/Logout'
@@ -12,6 +13,7 @@ import Logout from './Components/Logout/Logout'
 ReactDOM.render(
 	<React.StrictMode>
 		<Router>
+            <Route exact path="/" component={Home} />
             <Route exact path="/game" component={Game} />
             <Route exact path="/login" render={(props) => <AuthenticationModal form="login" />} />
             <Route exact path="/register" render={(props) => <AuthenticationModal form="register" />} />
