4f044c95efe2e842c61fb84f145e1bd7683f564b
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2020-04-20 06:52:22 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+// Deploy test.
 import 'reset-css'
 import './index.css'
 
